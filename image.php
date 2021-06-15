18be3a337e21f58b30b17d92e788f4e1c872ee44
Owen <houessouowen@gmail.com>
2021-06-15 11:31:10 +0000

    debut suivi

---
+++ image.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Images</title>
+  <link rel="stylesheet" href="style.css" type="text/css" />
+  <script type="text/javascript" src="main.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0/css/fontawesome.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0/css/brands.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0/css/solid.min.css">
+</head>
+
+<body>
+  <header class="header">
+    <h1>Weecode</h1>
+    <p> Apparrence a voir</p>
+    <div class="topnav">
+      <a class="active" href="index.html">Home</a>
+      <a href="news.html">News</a>
+      <a href="login.html">Login</a>
+      <a href="image.html">Images</a>
+      <a href="register.html">Sign-up</a>
+    </div>
+  </header>
+  <main class="container">
+    <h2>tsun</h2>
+
+    <h3>chambre blanche et grise</h3>
+    <div class="slider-scroll">
+      <img id="slide-scroll1" src="https://previews.123rf.com/images/demkat/demkat1702/demkat170200163/72961783-int%C3%A9rieur-de-chambre-blanche-et-grise.jpg" alt>
+      <h3>rieur hardwood</h3>
+      <img id="slide-scroll2" src="https://previews.123rf.com/images/breadmaker/breadmaker1511/breadmaker151100093/50550100-salle-%C3%A0-manger-int%C3%A9rieur-avec-hardwood-floors-%C3%A0-new-luxury-home.jpg" alt>
+      <h3>intérieur blanc</h3>
+      <img id="slide-scroll3" src="https://previews.123rf.com/images/bialasiewicz/bialasiewicz1612/bialasiewicz161201097/67613440-maison-int%C3%A9rieur-blanc-avec-fauteuil-vert-et-mur-de-briques.jpg" alt>
+      <h3>mur de briques</h3>
+      <img id="slide-scroll4" src="https://previews.123rf.com/images/breadmaker/breadmaker1511/breadmaker151100048/50557141-belle-pi%C3%A8ce-de-vie-avec-des-planchers-de-bois-franc-et-vue-imprenable.jpg" alt>
+    </div>
+    <ul class="ancre">
+      <li><a href="#slide-scroll1">1</a></li>
+      <li><a href="#slide-scroll2">2</a></li>
+      <li><a href="#slide-scroll3">3</a></li>
+      <li><a href="#slide-scroll4">4</a></li>
+    </ul>
+  </main>
+  <footer class="footer">
+
+    <div class="reseaux">
+      <p>Réseaux sociaux</p>
+      <ul>
+        <li><a href="facebook.com" class="fab fa-facebook-f"> Facebook</a></li>
+        <li><a href="instagram.com" class="fab fa-instagram"> Instagram</a></li>
+        <li><a href="pinterest.com" class="fab fa-pinterest"> Pinterest</a></li>
+      </ul>
+    </div>
+    <div class="contact">
+      <p>Contact</p>
+      <ul>
+        <li>adresse</li>
+        <li>code postal</li>
+        <li>ville</li>
+        <li>numéro de téléphone</li>
+        <li>email</li>
+      </ul>
+    </div>
+    <div class="lorem">
+      <p>lettres</p>
+      <ul>
+        <li>j</li>
+        <li>y</li>
+        <li>m</li>
+      </ul>
+    </div>
+  </footer>
+</body>
+
+</html>
