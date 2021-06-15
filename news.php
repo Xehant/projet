f4af16e1b1bd15a44334c751d34b995930b701f9
Owen <houessouowen@gmail.com>
2021-06-15 11:36:14 +0000

    debut suivoi

---
+++ news.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>register</title>
+  <link rel="stylesheet" href="style.css" type="text/css" />
+  <script type="text/javascript" src="main.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0/css/fontawesome.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0/css/brands.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0/css/solid.min.css">
+</head>
+
+<body>
+  <header class="header">
+    <h1>Weecode</h1>
+    <p> Apparrence a voir</p>
+    <div class="topnav">
+      <a class="active" href="index.php">Home</a>
+      <a href="news.php">News</a>
+      <a href="login.php">Login</a>
+      <a href="image.php">Images</a>
+      <a href="register.php">Sign-up</a>
+    </div>
+  </header>
+  <main class="container">
+
+    <p>4</p>
+    <p>7</p>
+    <p>8</p>
+  </main>
+  <footer class="footer">
+
+    <div class="reseaux">
+      <p>Réseaux sociaux</p>
+      <ul>
+        <li><a href="facebook.com" class="fab fa-facebook-f"> Facebook</a></li>
+        <li><a href="instagram.com" class="fab fa-instagram"> Instagram</a></li>
+        <li><a href="pinterest.com" class="fab fa-pinterest"> Pinterest</a></li>
+      </ul>
+    </div>
+    <div class="contact">
+      <p>Contact</p>
+      <ul>
+        <li>adresse</li>
+        <li>code postal</li>
+        <li>ville</li>
+        <li>numéro de téléphone</li>
+        <li>email</li>
+      </ul>
+    </div>
+    <div class="lorem">
+      <p>lettres</p>
+      <ul>
+        <li>j</li>
+        <li>y</li>
+        <li>m</li>
+      </ul>
+    </div>
+  </footer>
+</body>
